d560c08c7c663042ee89acc7085125e39b385431
daulen <daulenz@gmail.com>
2021-12-21 10:51:05 -0600

    commit tree 082a740eeabf8156a77c8357975fb4609302b7d6

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-function findMinAndRemove(array){
+let unsortedArray = [5, 6, -1, 1, 3];
 
+function minAndRemove(array) {
+	let min = array[0];
+	let minIndex = 0;
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] < min) {
+			min = array[i];
+			minIndex = i;
+		}
+	}
+	array.splice(minIndex, 1);
+  console.log(min);
+	return min;
 }
+// console.log(minAndRemove([7,3,0,9,5]));
 
-function selectionSort(array){
-
+function selectionSort(array) {
+	let newMin;
+	let sorted = [];
+	while (array.length != 0) {
+		newMin = minAndRemove(array);
+		sorted.push(newMin);
+	}
+	return sorted;
 }
+console.log(selectionSort(unsortedArray));
